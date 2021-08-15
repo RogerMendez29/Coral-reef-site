bb82b046b01bd56910b030dcc482fc32efaf5b18
Roger Mendez <roger61087@gmail.com>
2021-08-15 13:08:54 -0400

    added js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-const h2 = document.createElement("h2");
-h2.textContent = "New Caledonia Barrier Reef";
-document.querySelector ("body").appendChild(h2);
 
+const text = document.querySelector(".intro");
+const stripText = text.textContent;
+const splitText = stripText.split("");
+text.textContent = "";
+
+for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+ 
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const span = text.querySelectorAll("span")[char];
+    span.classList.add("fade");
+    char++
+    if(char === splitText.length){
+        complete();
+        return;
+    }
+}
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
